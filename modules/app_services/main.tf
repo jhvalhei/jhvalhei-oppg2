bc84ca805905755898ac906b0ec6aed578aaa31a
JensHerman <jhvalhei@stud.ntnu.no>
2024-11-13 01:01:54 +0100

    added appservice module reference

--- modules/app_services/main.tf
+++ modules/app_services/main.tf
@@ -1,5 +1,12 @@
+locals {
+  workspaces_suffix = terraform.workspace == "default" ? "" : "${terraform.workspace}"
+
+  sp_name = "${var.sp_name}-${local.workspaces_suffix}"
+
+}
+
 resource "azurerm_service_plan" "sp" {
-  name                = var.sp_name
+  name                = local.sp_name
   resource_group_name = var.rg_name
   location            = var.location
   os_type             = "Linux"
